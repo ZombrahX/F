af828c1b49a3f6a25ef0e2d521ad2f8c53fd48bc
ZombrahX <ashco214@gmail.com>
2025-07-16 21:19:52 -0500

    Add files via upload

---
+++ Final/Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by user277142 on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Final/FinalTests/FinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalTests.swift
+//  FinalTests
+//
+//  Created by user277142 on 7/16/25.
+//
+
+import Testing
+@testable import Final
+
+struct FinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
